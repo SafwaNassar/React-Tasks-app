1ce0ca8ebb63be795f9c5582707354339d05ed4b
Safwa Omar Nassar <somar@brightskiesinc.local>
2020-06-14 09:19:51 +0200

    Updates

---
+++ precache-manifest.961f96967bb5abc1c0f1f91327260a20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae3a430e16d863a2115583f998bac9e",
+    "url": "/React-Tasks-app/index.html"
+  },
+  {
+    "revision": "3f6b5f3569b1a7ce9a62",
+    "url": "/React-Tasks-app/static/css/main.1a39a173.chunk.css"
+  },
+  {
+    "revision": "4c60f12f92f858a01b6f",
+    "url": "/React-Tasks-app/static/js/2.21cde119.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Tasks-app/static/js/2.21cde119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6b5f3569b1a7ce9a62",
+    "url": "/React-Tasks-app/static/js/main.6658fd03.chunk.js"
+  },
+  {
+    "revision": "2a30fe20f38667aa1134",
+    "url": "/React-Tasks-app/static/js/runtime-main.4c90e60e.js"
+  }
+]);
